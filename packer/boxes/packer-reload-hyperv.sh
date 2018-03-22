9f81535ebbae0c737a192e7e24d3fc12748d9963
Brennan Fee <bfee@uship.com>
2018-03-22 15:48:51 -0500

    Adding HyperV versions of vagrant scripts.

--- packer/boxes/packer-reload-hyperv.sh
+++ packer/boxes/packer-reload-hyperv.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 if [[ -f bfee-arch-bootstrapped-hyperv.box ]]; then
-    vagrant box remove bfee-arch-bootstrapped-hyperv
-    vagrant box add bfee-arch-bootstrapped-hyperv.box --name bfee-arch-bootstrapped-hyperv
+    vagrant.exe box remove bfee-arch-bootstrapped-hyperv
+    vagrant.exe box add bfee-arch-bootstrapped-hyperv.box --name bfee-arch-bootstrapped-hyperv
 fi
