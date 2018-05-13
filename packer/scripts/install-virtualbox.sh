10246e9c8be696e18e36a0e40452215ec431b3b4
Brennan Fee <github@todaytechsoft.biz>
2018-05-13 01:19:19 -0500

    Fixing virtualbox install script
    Started getting an error during virutalbox installation after yaourt
    removal.  The pacman packages were out of date so I'm adding an explicit
    sync during the VirtualBox guest install.

--- packer/scripts/install-virtualbox.sh
+++ packer/scripts/install-virtualbox.sh
@@ -6,8 +6,8 @@ IFS=$'\n\t'
 
 # VirtualBox Guest Additions
 # https://wiki.archlinux.org/index.php/VirtualBox
-/usr/bin/pacman -S --noconfirm virtualbox-guest-dkms virtualbox-guest-utils
-echo -e 'vboxguest\nvboxsf\nvboxvideo' > /etc/modules-load.d/virtualbox.conf
+/usr/bin/pacman -Syu --noconfirm virtualbox-guest-dkms virtualbox-guest-utils
+echo -e 'vboxguest\nvboxsf\nvboxvideo' >/etc/modules-load.d/virtualbox.conf
 
 /usr/bin/systemctl enable vboxservice.service
 
