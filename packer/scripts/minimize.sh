554cbd0a8bf8d24b66ce3933676b50992bf9e382
Brennan Fee <brennan@github.mailbots.org>
2020-02-29 19:49:34 -0600

    Updates to packer script
    Updated to support latest Arch and Packer

--- packer/scripts/minimize.sh
+++ packer/scripts/minimize.sh
@@ -2,6 +2,6 @@
 
 # Write zeros to improve virtual disk compaction
 zerofile=$(/usr/bin/mktemp /zerofile.XXXXX)
-/usr/bin/dd if=/dev/zero of="$zerofile" bs=1M
+/usr/bin/dd if=/dev/zero of="$zerofile" bs=1M 2> /dev/null
 /usr/bin/rm -f "$zerofile"
 /usr/bin/sync
